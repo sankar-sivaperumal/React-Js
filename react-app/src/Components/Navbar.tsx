233b54101af1f41f4fe16ec3bb62588f1c5dd45f
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-03 18:10:08 +0530

    Updated new values

--- react-app/src/Components/Navbar.tsx
+++ react-app/src/Components/Navbar.tsx
@@ -1,37 +1,25 @@
+import { Link } from 'react-router-dom';
 
  function Navbar() {
     return (
-        <>
-    <nav className="navbar navbar-expand-sm bg-light">
+   <>
+    <nav className="navbar navbar-expand-sm bg-light mx-1  my-1 px-1">
       <div className="container-fluid">
-       <ul className="navbar-nav">
-      <li className="nav-item">
-        <a className="nav-link" href= "Header.tsx">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="Gallery.tsx">Gallery</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href=".Footer.tsx">Contact</a>
-      </li>
-    </ul>
-    <form className="d-flex">
-        <input className="form-control me-2" type="text" placeholder="Search"></input>
-        <button className="btn btn-primary" type="button">Search</button>
-      </form>
-  </div>
+        <ul className="navbar-nav">
+          <li><Link className="nav-link" to= "/Header" >Home</Link></li> 
+          <li><Link className="nav-link" to="/Gallery">Gallery</Link></li>   
+          <li><Link className="nav-link" to="/Contact">Contact</Link></li>
+        </ul>
 
-</nav>
-</>
+        <form className="d-flex">
+         <input className="form-control me-2" type="text" placeholder="Search"></input>
+         <button className="btn btn-primary" type="button">Search</button>
+
+        </form>
+     </div>
+    </nav>
+  </>
   );
 }
 
 export default Navbar;
-
- 
-
-
-
-
-
-  
