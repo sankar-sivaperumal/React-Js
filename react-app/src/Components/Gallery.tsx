deb1f076d7580100ae9ebb887043f03baff5eac3
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-03 12:13:31 +0530

    change the view in row

--- react-app/src/Components/Gallery.tsx
+++ react-app/src/Components/Gallery.tsx
@@ -6,28 +6,32 @@ import '../App.css'
 export default function Gallery() {
   return (
     <>
-      <div className="card" style={{ width: '18rem' }}>
+  
+ <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+
+      <div className="card" style={{ width: '18rem', margin:"3rem",textAlign:"center"}}>
         <img src={lion} className="card-img-top" alt="Lion" />
         <div className="card-body">
           <h3 className="card-title">Lion</h3>
           <p className="card-text">The majestic lion, the king of the jungle.</p>
         </div>
-      </div>
+        </div>
 
-      <div className="card" style={{ width: '18rem' }}>
+        <div className="card" style={{ width: '18rem', margin:"3rem",textAlign:"center"}}>
         <img src={peacock} className="card-img-top" alt="Peacock Feather" />
         <div className="card-body">
           <h3 className="card-title">Peacock Feather</h3>
           <p className="card-text">A beautiful peacock feather with vivid colors.</p>
         </div>
-      </div>
-
-      <div className="card" style={{ width: '18rem' }}>
-        <img src={cat} className="card-img-top" alt="Cat" />
-        <div className="card-body">
-          <h3 className="card-title">Cat</h3>
-          <p className="card-text">A cute and curious cat with soft fur.</p>
         </div>
+        
+      <div className="card" style={{ width: '18rem', margin:"3rem",textAlign:"center"}}>
+           <img src={cat} className="card-img-top" alt="Cat" />
+          <div className="card-body">
+           <h3 className="card-title">Cat</h3>
+           <p className="card-text">A cute and curious cat with soft fur.</p>
+        </div>
+      </div>
       </div>
     </>
   );
