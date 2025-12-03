d0a456c353618e06764f9d61dbc7d2dcb3108db6
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-03 12:12:16 +0530

    Added buttons, contact info, Address

--- react-app/src/Components/Footer.tsx
+++ react-app/src/Components/Footer.tsx
@@ -1,12 +1,33 @@
 function Footer(){
   return (
-    <footer className="footer">
-      <div className="link">
-      <p>&copy;  React</p>
-          <a href="https://instagram.com" target="_blank" rel="">Instagram</a>
-  </div>
-    </footer>
-  );
-};
+    <>
+  <footer className="footer bg-light text-dark py-3 fixed-bottom">
+  <div className="container d-flex justify-content-between align-items-center">
 
-export default Footer;
+    <div className="text-start">
+      <p className="text-left"><strong>Email:</strong> gallery@email.com</p>
+      <p className="text-left"><strong>Phone:</strong> 044-999-666</p>
+    </div>
+  
+    <div className="text-center">
+      <p className="text m-0 mb-2">&copy; <strong>React</strong></p>
+
+      <div className="d-flex gap-2">
+        <a className="btn btn-info" href="https://instagram.com">Instagram</a>
+        <a className="btn btn-primary" href="https://facebook.com">Facebook</a>
+        <a className="btn btn-danger" href="https://youtube.com">YouTube</a>
+      </div>
+    </div>
+   
+    <div className="text-end">
+      <p><strong>Address:</strong></p>
+      <p >10, Beach Road Chennai</p>
+    </div>
+
+  </div>
+</footer>
+</>
+);
+}
+    
+ export default Footer;
