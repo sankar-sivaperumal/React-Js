a63cd17b4da9a415e7584bd54693fa2bd8876fdd
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-04 15:50:30 +0530

    Updated contact code

--- react-app/src/Components/Contact.tsx
+++ react-app/src/Components/Contact.tsx
@@ -1,4 +1,4 @@
-import './index.css'
+
 function Contact() {
   return (
     <>
@@ -37,30 +37,3 @@ function Contact() {
 
 export default Contact;
  
-{/*    <form>
-    <div className="container mt-5"style={{maxWidth:"60%",padding: "15px" ,width:"40%"}}>
-      <h1>Contact Page</h1>
-
-    <div className="mb-3">
-      <label htmlFor="email" className="form-label">Email address</label>
-      <input type="email" className="form-control" id="email" placeholder="you@example.com" />
-    </div>
-
-     <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
-        <input type="text" className="form-control" id="name" placeholder="Name" />
-    </div>
-
-    <div className ="mb-3" >
-        <label htmlFor="Phone" className="form-label">Phone</label>
-        <input type="telephone" className="form-control" id="phone"  placeholder="Phone" />
-    </div>
-
-    <div className="mb-3">
-      <label htmlFor="message" className="form-label">Message</label>
-      <textarea className="form-control" id="message" rows={4}></textarea>
-    </div>
-
-    <button type="submit" className="btn btn-primary">Submit</button>
-   </div>
-  </form> */}
