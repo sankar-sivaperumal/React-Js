07304bbeefccef75adf8b91b3848a9f698fc54ce
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-08 11:09:22 +0530

    Added new component

--- react-app/src/Components/Navbar.tsx
+++ react-app/src/Components/Navbar.tsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
  function Navbar() {
     return (
    <>
-    <nav className="navbar navbar-expand-sm bg-light mx-1  my-1 px-1">
+    <nav className="navbar navbar-expand-sm bg-light">
       <div className="container-fluid">
         <ul className="navbar-nav">
           <li><Link className="nav-link" to= "/Header" >Home</Link></li> 
           <li><Link className="nav-link" to="/Gallery">Gallery</Link></li>   
           <li><Link className="nav-link" to="/Contact">Contact</Link></li>
+           <li><Link className="nav-link" to="/JsonTable">JSON</Link></li>
         </ul>
       </div>
     </nav>
