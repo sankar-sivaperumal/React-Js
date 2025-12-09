73371c86d68147c21dc3168abca60a91c4fe4d2e
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-09 16:54:54 +0530

    updated the layout

--- react-app/src/Components/Layout.tsx
+++ react-app/src/Components/Layout.tsx
@@ -7,25 +7,22 @@ import Home from './home';
 import JsonTable from './jsondata';
 
 
-
-
-
-function Main() {
+export default function Main() {
   return (
     <Router>
       <Navbar />
       <div className="container mt-3">
         <Routes>
+          
           <Route path="/Header" element={<Home />} />
+          
           <Route path="/Gallery" element={<Gallerys />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/JsonTable" element={<JsonTable />} />
         </Routes>
       </div>
-      
       <Footer />
     </Router>
   );
 }
 
-export default Main;
