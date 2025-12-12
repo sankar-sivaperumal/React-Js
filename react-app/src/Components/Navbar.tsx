efc56eea604202ca569f3e1cc7f83f05e4af0a20
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-12 11:11:36 +0530

    Changed path file name

--- react-app/src/Components/Navbar.tsx
+++ react-app/src/Components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
     <nav className="navbar navbar-expand-sm bg-light">
       <div className="container-fluid">
         <ul className="navbar-nav">
-          <li><Link className="nav-link" to= "/Header" >Home</Link></li> 
+          <li><Link className="nav-link" to= "/Home" >Home</Link></li> 
           <li><Link className="nav-link" to="/Gallery">Gallery</Link></li>   
           <li><Link className="nav-link" to="/Contact">Contact</Link></li>
            <li><Link className="nav-link" to="/JsonTable">JSON</Link></li>
