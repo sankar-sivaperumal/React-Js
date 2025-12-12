7a2c62be9de7be67992fa5550de962d513675971
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-12 11:12:56 +0530

    Changed file path name

--- react-app/src/Components/Layout.tsx
+++ react-app/src/Components/Layout.tsx
@@ -1,28 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Gallerys from './Gallery';
-import Contact from './Contact';
-import Footer from './Footer';
-import Home from './home';
-import JsonTable from './jsondata';
-
+import Navbar from './Navbar';  
+import Gallery from '../Components/testing tools/gallery';  
+import Contact from './Contact';  
+import Footer from './Footer';  
+import Home from '../Components/testing tools/home';  
+import JsonTable from './jsondata';  
 
 export default function Main() {
   return (
     <Router>
-      <Navbar />
-      <div className="container mt-3">
+      <Navbar />  
+      <div className="container mt-3">  
         <Routes>
-          
-          <Route path="/Header" element={<Home />} />
-          
-          <Route path="/Gallery" element={<Gallerys />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/JsonTable" element={<JsonTable />} />
+          <Route path="/Home" element={<Home />} />  
+          <Route path="/Gallery" element={<Gallery />} /> 
+          <Route path="/Contact" element={<Contact />} />  
+          <Route path="/JsonTable" element={<JsonTable />} />  
         </Routes>
       </div>
-      <Footer />
+      <Footer />  
     </Router>
   );
 }
-
