5ab8480d28de5a80b02e189d7b9a88bf4c23c662
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-12 12:58:29 +0530

    updated redux file and wrap by provider

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/* import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
@@ -7,4 +7,18 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+) */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';  
+import { store } from './Components/Redux toolkit/store.tsx';  
+import App from './App.tsx';
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>  
+      <App />
+    </Provider>
+  </StrictMode>,
+);
