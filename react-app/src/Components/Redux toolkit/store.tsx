854e82a7dc0f839a7d49988f64c462bc71313204
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-12 12:57:47 +0530

    created redux store file

---
+++ react-app/src/Components/Redux toolkit/store.tsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import imageReducer from './imageSlice';
+
+export const store = configureStore({
+  reducer: {
+    images: imageReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
